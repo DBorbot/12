f6bf0bd22957b80f1d202af64a1e2f88e5f88612
Dima <Borbot.D@gmail.com>
2025-03-02 14:44:37 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,74 @@
+"use strict"
+
+// console.log(1);
+
+// alert(`Hello, World!`);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("How old are you?", "");
+// console.log(answer + 15);
+
+// let answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your surname?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// console.log(typeof (answers));
+
+// let category = "toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+
+// let user = "dimas";
+// alert(`Hello ${user}`);
+
+// console.log("string text line 1\n" + "string text line 2\n" + "string text line 3");
+
+// console.log(`    ${`string text line 1`}
+//     ${`string text line 2`}
+//     ${`string text line 3`}`);
+
+// let a = 5;
+// let b = 10;
+// console.log("Feefteen is " + (a + b) + " and not " + (2 * a + b) + ".");
+
+// let c= 5;
+// let d = 10;
+// console.log(`Feefteen is ${c + d} and not ${2 * c + d}.`);
+
+// console.log(+"4" + +"5");     /* dis is a number */
+
+// console.log(5 + +"5");    /* dis is a number too */
+
+// +cosole.log("5" + "5");     /* dis is a string */
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--;     /*-- postfix increment and decrement*/
+
+// // ++incr;
+// // --decr;     /*-- prefix increment and decrement*/
+
+// console.log(++incr);     /*  11 */
+// console.log(--decr);     /*  9 */
+
+// console.log(incr++);     /* 10 */
+// console.log(decr--);     /* 10 */
+
+
+cosole.log(5 % 2);       /* 1 */
+console.log(2 * 4 == 8);     /* true */
+console.log(2 * 4 == "8");    /* true */
+console.log(2 * 4 === "8");   /* false */
+console.log(2 * 4 === 8);     /* true */
+
+let biedronka = true
+lidl = false;
+console.log(biedronka && lidl);
+

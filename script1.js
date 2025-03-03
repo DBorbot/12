52fd10f3996765074be2710b8f7f3f08cd0507eb
Dima <Borbot.D@gmail.com>
2025-03-03 16:25:52 +0100

    index.html mod

--- script1.js
+++ script1.js
@@ -14,17 +14,47 @@
 // };
 // console.log(power(2, 10));
 
-let x = "Outside";
-let f1 = function () {
-    let x = "inside f1";
+// let x = "Outside";
+// let f1 = function () {
+//     let x = "inside f1";
 
-};
-f1();
-console.log(x);
+// };
+// f1();
+// console.log(x);
 
-let f2 = function () {
+// let f2 = function () {
 
-    x = "inside f2";
-};
-f2();
-console.log(x)
+//     x = "inside f2";
+// };
+// f2();
+// console.log(x)
+
+// var landscape = function () {
+
+//     var result = "";
+//     var flat = function (size) {
+//         for (var count = 0; count < size; count++)
+//             result = (result + "_")
+        
+//     };
+
+//     var mountain = function (size) {
+//         result = (result + "/")
+//         for (var count = 0; count < size; count++)
+//             result = (result + "`")
+//         result = (result + "\\")
+    
+
+//     };
+
+//     flat(9);
+//     mountain(4);
+//     flat(6);
+//     mountain(1);
+//     flat(1);
+//     return result;
+
+// };
+// console.log(landscape());
+
+

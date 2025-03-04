4bc834e04788784abe0262a397de39ab480624b3
Dima <Borbot.D@gmail.com>
2025-03-04 17:10:21 +0100

    add js/alertConfirmPrompt/         js/landscape/         js/logicalOperators/         js/personalMoviesDB/

---
+++ js/landscape/landscape.js
@@ -0,0 +1,64 @@
+
+
+
+// let makeNoise = function () {
+//     console.log("pling!");
+// };
+// makeNoise();
+
+// var power = function (base, exponent) {
+//     var result = 1;
+//     for (var count = 0; count < exponent; count++)
+//         result = result * base;
+//     return result;
+// };
+// console.log(power(2, 10));
+
+// let x = "Outside";
+// let f1 = function () {
+//     let x = "inside f1";
+
+// };
+// f1();
+// console.log(x);
+
+// let f2 = function () {
+
+//     x = "inside f2";
+// };
+// f2();
+// console.log(x)
+
+
+const land = ("Хочеш запустить программу \"ЛАНДШАФТ\"", "");
+
+var landscape = function () {
+
+    var result = "";
+    var flat = function (size) {
+        for (var count = 0; count < size; count++)
+            result = (result + "_")
+
+    };
+
+    var mountain = function (size) {
+        result = (result + "/")
+        for (var count = 0; count < size; count++)
+            result = (result + "`")
+        result = (result + "\\")
+
+
+    };
+
+    flat(9);
+    mountain(4);
+    flat(6);
+    mountain(1);
+    flat(1);
+    return result;
+
+};
+console.log(landscape());
+
+
+

ca3bb4b7191a5497173edff3bbcdffe748adf634
Dima <Borbot.D@gmail.com>
2025-03-06 21:54:45 +0100

    Add 1 exercise from Haverbeke Vyrazitelnij javascript

--- script.js
+++ script.js
@@ -1,33 +1,118 @@
-console.log("Yo are Winner!!!");
+// console.log("Yo are Winner!!!");
 
-const land = prompt("Хочеш запустить программу \"ЛАНДШАФТ\"", "");
-if (land == "Да", land == "да") {
+// const land = prompt("Хочеш запустить программу \"ЛАНДШАФТ\"", "");
+// if (land == "Да", land == "да") {
 
-    var landscape = function () {
+//     var landscape = function () {
 
-        var result = "";
-        var flat = function (size) {
-            for (var count = 0; count < size; count++)
-                result = (result + "_")
+//         var result = "";
+//         var flat = function (size) {
+//             for (var count = 0; count < size; count++)
+//                 result = (result + "_")
 
-        };
+//         };
 
-        var mountain = function (size) {
-            result = (result + "/")
-            for (var count = 0; count < size; count++)
-                result = (result + "`")
-            result = (result + "\\")
+//         var mountain = function (size) {
+//             result = (result + "/")
+//             for (var count = 0; count < size; count++)
+//                 result = (result + "`")
+//             result = (result + "\\")
 
 
-        };
+//         };
 
-        flat(9);
-        mountain(4);
-        flat(6);
-        mountain(1);
-        flat(1);
-        return result;
+//         flat(9);
+//         mountain(4);
+//         flat(6);
+//         mountain(1);
+//         flat(1);
+//         return result;
 
-    };
-    console.log(landscape());
+//     };
+//     console.log(landscape());
+// };
+
+
+
+// function isEven(N) {
+//     if ((N % 2) == 0 && (N - 2) % 2 == 0)
+//         return true;
+//     else return false;
+// };
+// console.log(isEven(1011));
+
+function countBs(b, a, c) {
+
+    if (a == a)
+        return console.log(b.charAt(a))
+    else if (b == b)
+        return console.log(b.length)
+    else
+        return "Error!!!";
+
+
+
+
+
 };
+console.log(countBs("tuqewtuuiyrqrqouroiuoqu", 5));
+
+// let charAt = [1, 2, 3, 4,];
+// console.log(charAt.length - 1);
+// charAt.push(5);
+// console.log(Object.keys(charAt));
+// console.log(Object.entries(charAt));
+// charAt.splice(4, 0, "Привет");
+// console.log(Object.entries(charAt));
+// console.log()
+
+
+
+
+
+
+
+// "use strict"
+
+// if (4 == 9) {
+//     console.log("ok");
+// } else {
+//     console.log("Error");
+// }
+
+// const num = 50;
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log("Много");
+// } else {
+//     console.log(`Ok`);
+// }
+
+// const num = 5;
+// (num === 50) ? console.log(`Ok`) : console.log(`Error`);
+
+// const num = 30;
+// switch (num) {
+//     case 49:
+//         console.log(`Неверно`);
+//         break;
+//     case 100:
+//         console.log(`Неверно`);
+//         break;
+//     case 50:
+//         console.log(`В точку`);
+//         break;
+//     default:
+//         console.log(`Не в этот раз`);
+//         break;
+// }
+
+// const hamburger = true;
+// const fries = true;
+
+// if (hamburger && fries) {
+//     console.log("i'm done")
+// };
+
+
